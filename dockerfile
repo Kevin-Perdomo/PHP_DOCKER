76184c6475082137978e8f0882a34b842988a2b7
Kevin Siqueira Perdomo <kevin.siqueira23@gmail.com>
2024-09-29 09:36:03 -0300

    teste

--- dockerfile
+++
@@ -1,8 +0,0 @@
-# Use a imagem do PHP com Apache como base
-FROM php:8.1-apache-buster
-
-# Exponha a porta 80 do contêiner
-EXPOSE 80
-
-# Copie o conteúdo da pasta src para o diretório padrão do Apache no contêiner
-#COPY src/ /var/www/html/ 
---
+++ projeto_coppam/php/cadastro.php
@@ -0,0 +1,32 @@
+<?php
+// Dados de conexão com o banco de dados
+$servername = "db";
+$username = "meu_usuario";
+$password = "senha_usuario";
+$dbname = "meu_banco";
+
+// Criando conexão com o MySQL
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verificar conexão
+if ($conn->connect_error) {
+    die("Conexão falhou: " . $conn->connect_error);
+}
+
+// Capturar dados do formulário
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+$senha = password_hash($_POST['senha'], PASSWORD_DEFAULT);
+
+// Inserir dados na tabela
+$sql = "INSERT INTO usuarios (nome, email, senha) VALUES ('$nome', '$email', '$senha')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Cadastro realizado com sucesso!";
+} else {
+    echo "Erro: " . $sql . "<br>" . $conn->error;
+}
+
+// Fechar conexão
+$conn->close();
+?>
